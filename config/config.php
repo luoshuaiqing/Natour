62e018369e07a0c86c7409691691bc08c8cacbe8
luoshuaiqing <shuaiqin@usc.edu>
2020-04-15 18:18:24 -0700

    update db name

--- config/config.php
+++ config/config.php
@@ -2,5 +2,5 @@
 	define("DB_HOST","460.itpwebdev.com");
 	define("DB_USER","bo_natour_user");
 	define("DB_PASS","itp460natour");
-	define("DB_NAME","bo_natour_user");
+	define("DB_NAME","bo_natour_db");
 ?>
